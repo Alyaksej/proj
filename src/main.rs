1bd7041072d4b2c1dfd6e5120e6d3ad3723d749e
aliaksei <alex.circuit.soft@gmail.com>
2024-04-24 18:43:19 +0300

    add socket

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,32 @@
 extern crate libc;
 use std::os::raw::c_int;
-use std::os::unix::io::AsRawFd;
 use std::os::unix::net::{UnixDatagram};
-use std::io;
 
 extern {
     fn arrayProcessing (ptr_in: *mut c_int, n: c_int) -> *mut c_int;
 }
-
+const SOCKET_PATH: &str = "/../../../../tmp/socket.sock";
 fn main() {
-    let socket = UnixDatagram::bind("/tmp/socket.sock");
+    //  Create unix_socket
+    let socket = UnixDatagram::bind(SOCKET_PATH).unwrap();
     let mut data_array: Vec<i32> = Vec::new();
-    let mut buffer = [0; 4]; // for byte to i32
-    let m = socket.recv(&mut buffer);
-    let received_data = i32::from_ne_bytes(buffer);
-    data_array.push(received_data);
 
-    // let mut array = [1, 2, 3, 4, 5];
-    // let ptr = array.as_mut_ptr();
-    // let n = array.len() as c_int;
-    //
-    // unsafe {
-    //     let result = arrayProcessing(ptr, n);
-    //     for i in 0..n {
-    //         println!("{}", *result.offset(i.try_into().unwrap()));
-    //     }
-    // }
+    loop {
+        let mut buffer = [0u8; 4]; // for byte to i32
+        let _ = socket.recv_from(&mut buffer);
+        let received_data = i32::from_be_bytes(buffer);
+        data_array.push(received_data);
 
-    let ptr = data_array.as_mut_ptr();
-    let n = data_array.len() as c_int;
-    if data_array.len() >= 20 {
-        unsafe {
-            let result = arrayProcessing(ptr, n);
-            for i in 0..n {
-                println!("{}", *result.offset(i.try_into().unwrap()));
+        let ptr = data_array.as_mut_ptr();
+        let n = data_array.len() as c_int;
+        if data_array.len() >= 20 {
+            unsafe {
+                let result = arrayProcessing(ptr, n);
+                for i in 0..n {
+                    println!("{}", *result.offset(i.try_into().unwrap()));
+                }
             }
+            data_array.clear();
         }
     }
 }
