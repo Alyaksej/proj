8f222ea6162da2482879a81ca821ca53838885f0
aliaksei <alex.circuit.soft@gmail.com>
2024-04-26 19:01:37 +0300

    add errors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate libc;
 use std::os::raw::c_int;
 use std::os::unix::net::{UnixDatagram};
+use std::convert::TryInto;
+use std::fs;
 
 extern {
     fn arrayProcessing (ptr_in: *mut c_int, n: c_int) -> *mut c_int;
@@ -8,18 +10,27 @@ extern {
 const SOCKET_PATH: &str = "/../../../../tmp/socket.sock";
 fn main() {
     //  Create unix_socket
-    let socket = UnixDatagram::bind(SOCKET_PATH).unwrap();
+    let socket = match UnixDatagram::bind(SOCKET_PATH) {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("error binding socket: {}", e);
+            return;
+        }
+    };
     let mut data_array: Vec<i32> = Vec::new();
 
     loop {
         let mut buffer = [0u8; 4]; // for byte to i32
-        let _ = socket.recv_from(&mut buffer);
+        if let Err(e) = socket.recv_from(&mut buffer) {
+            eprintln!("Error receiving data: {}", e);
+            continue;
+        };
         let received_data = i32::from_be_bytes(buffer);
         data_array.push(received_data);
 
         let ptr = data_array.as_mut_ptr();
         let n = data_array.len() as c_int;
-        if data_array.len() >= 20 {
+        if data_array.len() >= 5 {
             unsafe {
                 let result = arrayProcessing(ptr, n);
                 for i in 0..n {
@@ -27,6 +38,9 @@ fn main() {
                 }
             }
             data_array.clear();
+        }
+        if let Err(e) = fs::remove_file(SOCKET_PATH) {
+            eprintln!("Error removing socket file: {}", e);
         }
     }
 }
