e9c324401a068e5661b04ba0630af2eeb420eac8
aliaksei <alex.circuit.soft@gmail.com>
2024-05-02 18:39:03 +0300

    try buffer

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,16 @@ extern {
 }
 const SOCKET_PATH: &str = "/../../../../tmp/socket.sock";
 fn main() {
+    // Receive on number
     //  Create unix_socket
-    let socket = match UnixDatagram::bind(SOCKET_PATH) {
-        Ok(s) => s,
-        Err(e) => {
-            eprintln!("error binding socket: {}", e);
-            return;
-        }
-    };
-    let mut data_array: Vec<i32> = Vec::new();
+    // let socket = match UnixDatagram::bind(SOCKET_PATH) {
+    //     Ok(s) => s,
+    //     Err(e) => {
+    //         eprintln!("error binding socket: {}", e);
+    //         return;
+    //     }
+    // };
+    // let mut data_array: Vec<i32> = Vec::new();
     //let mut buffer = vec![0u8, BUFFER_SIZE as u8];
 
     // loop {
@@ -45,28 +46,23 @@ fn main() {
     //     }
     // }
 
+    // Receive array of numbers
+    let socket = match UnixDatagram::bind(SOCKET_PATH) {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("error binding socket: {}", e);
+            return;
+        }
+    };
+    let mut data_array: Vec<i32> = Vec::new();
+
     const MAX_NUMBERS: usize = 5;
     const BUFFER_SIZE: usize = 4 * MAX_NUMBERS;
-
     loop {
-        let mut buffer = vec![0u8, (BUFFER_SIZE as i32).try_into().unwrap()];
+        let mut buffer = vec![0; BUFFER_SIZE];
+        let _ = socket.recv(&mut buffer);
 
-        if let Err(e) = socket.recv_from(&mut buffer) {
-            eprintln!("Error receiving data: {}", e);
-            continue;
-        }
-        println!("buffer{}",buffer[0]);
-        let num_received_bytes = buffer.len();
-        let num_received_numbers = num_received_bytes / std::mem::size_of::<i32>();
-
-        for n in 0..num_received_numbers {
-            let start_index = n * std::mem::size_of::<i32>();
-            let end_index = (n + 1) * std::mem::size_of::<i32>();
-            let number_bytes = &buffer[start_index..end_index];
-
-            let received_data = i32::from_be_bytes(number_bytes.try_into().expect("Invalid byte sequence"));
-            data_array.push(received_data);
-        }
+        println!("buffer{:?}", buffer);
 
         let ptr = data_array.as_mut_ptr();
         let n = data_array.len() as c_int;
@@ -78,9 +74,6 @@ fn main() {
                     println!("{}", *result.offset(i.try_into().unwrap()));
                 }
             }
-
-            // Clearing of processed data
-            data_array.drain(0..num_received_numbers);
 
             if let Err(e) = fs::remove_file(SOCKET_PATH) {
                 eprintln!("Error removing socket file: {}", e);
