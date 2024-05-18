1b1779023169baf492c821c11fc69a0274495baa
aliaksei <alex.circuit.soft@gmail.com>
2024-05-18 16:33:58 +0300

    екн ешьук

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ extern crate libc;
 use std::os::raw::{c_int, c_void};
 use std::os::unix::net::{UnixDatagram};
 use std::convert::TryInto;
-use std::{fs, vec};
-use std::time::{Duration, Instant};
+use std::{fs, thread, vec};
+use std::time::{Duration, Instant, SystemTime};
 
 // extern {
 //     fn arrayProcessing (ptr_in: *mut c_int, n: c_int) -> *mut c_int;
@@ -32,22 +32,26 @@ fn main() {
         }
     };
 
-    const MAX_NUMBERS: usize = 10;
+    const MAX_NUMBERS: usize = 20;
     const BUFFER_SIZE: usize = 4 * MAX_NUMBERS;
     const BUFFER_THRESHOLD: usize = BUFFER_SIZE - 10;
+    // Buffer for receiving data
     let mut buffer = vec![0; BUFFER_SIZE];
     let lib_ptr = buffer.as_mut_ptr() as *mut c_void;
     let lib_len_max = buffer.len() as c_int;
     let mut buffer_offset: usize = 0;
+
+    let mut all_time = 0;
+
+    let start_time = Instant::now();
+    let now = SystemTime::now();
     loop {
-        // Start of time calculation
-        let start = Instant::now();
         println!("buffer: {:?}", buffer);
         let body_slice: &mut [u8] = &mut buffer[buffer_offset..];
         match socket.recv(body_slice) {
             Ok(len_recv) => {
             println!("len_recv: {:?}", len_recv);
-            if len_recv == body_slice.len() {
+            if len_recv > body_slice.len() {
                 println!("Error receiving data: data is to long");
                 return;
             };
@@ -66,16 +70,25 @@ fn main() {
                 println!("result: {}", *result.offset(i.try_into().unwrap()));
             }
         }
+        println!("*******************************************************************************");
+        all_time += start_time.elapsed().as_micros();
+        println!("all_time: {}", all_time);
+        if all_time / 1000_000_000 == 5 {
+            println!("ololo");
+            all_time = 0;
+        }
+
         if buffer_offset >= BUFFER_THRESHOLD {
             buffer.iter_mut().for_each(|x| *x = 0);
             buffer_offset = 0;
         }
         // End of time calculation
-        let duration = start.elapsed();
-        println!("Time elapsed in expensive_function() is: {:?}", duration);
+        //let duration = start.elapsed();
+        //println!("Time elapsed in expensive_function() is: {:?}", duration);
     }
 }
 
-fn server_bandwith () {
-
+fn server_bandwidth(cnt_bytes: usize) {
+    let mut string = "ololo";
+    println!("string: -------------- {}", cnt_bytes);
 }
