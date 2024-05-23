b0a0c10c67a54c2ce29b48277cd23e63e3df1cfc
aliaksei <alex.circuit.soft@gmail.com>
2024-05-23 15:02:41 +0300

    add timestamp

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 extern crate libc;
 use std::os::raw::{c_int, c_void};
 use std::os::unix::net::{UnixDatagram};
-use std::convert::TryInto;
-use std::{fs, thread, vec};
-use std::time::{Duration, Instant, SystemTime};
+use std::{fs, vec};
+use std::time::{Instant};
 
 // extern {
 //     fn arrayProcessing (ptr_in: *mut c_int, n: c_int) -> *mut c_int;
@@ -13,7 +12,7 @@ extern {
     fn byteToInt (ptr_in: *mut c_void, len: c_int) -> *mut c_int;
 }
 
-const SOCKET_PATH: &str = "/../../../../tmp/socket.sock";
+const SOCKET_PATH: &str = "/tmp/socket.sock";
 fn main() {
     // Remove socket before start
     if fs::metadata(SOCKET_PATH).is_ok() {
@@ -40,55 +39,50 @@ fn main() {
     let lib_ptr = buffer.as_mut_ptr() as *mut c_void;
     let lib_len_max = buffer.len() as c_int;
     let mut buffer_offset: usize = 0;
+    let mut cnt_recv = 0;
+    let mut whole_recv= 0;
+    // Timestamp
+    let mut now = Instant::now();
 
-    let mut all_time = 0;
-
-    let start_time = Instant::now();
-    let now = SystemTime::now();
     loop {
-        println!("buffer: {:?}", buffer);
         let body_slice: &mut [u8] = &mut buffer[buffer_offset..];
         match socket.recv(body_slice) {
             Ok(len_recv) => {
-            println!("len_recv: {:?}", len_recv);
             if len_recv > body_slice.len() {
                 println!("Error receiving data: data is to long");
                 return;
             };
             buffer_offset += len_recv;
+            cnt_recv += len_recv;
+            whole_recv += cnt_recv;
         },
             Err(e) => {
             eprintln!("Error receiving data: {:?}", e);
             return;
         }
         }
-        println!("buffer_offset: {:?}", buffer_offset);
-        println!("body_slice: {:?}", body_slice);
+
         unsafe {
-            let result = byteToInt(lib_ptr, lib_len_max);
-            for i in 0..MAX_NUMBERS {
-                println!("result: {}", *result.offset(i.try_into().unwrap()));
-            }
+            let _result = byteToInt(lib_ptr, lib_len_max);
+            // for i in 0..MAX_NUMBERS {
+            //     println!("result: {}", *result.offset(i.try_into().unwrap()));
+            // }
         }
-        println!("*******************************************************************************");
-        all_time += start_time.elapsed().as_micros();
-        println!("all_time: {}", all_time);
-        if all_time / 1000_000_000 == 5 {
-            println!("ololo");
-            all_time = 0;
+
+        if now.elapsed().as_secs() >= 5 {
+            server_bandwidth(cnt_recv, whole_recv);
+            cnt_recv = 0;
+            now = Instant::now();
         }
 
         if buffer_offset >= BUFFER_THRESHOLD {
             buffer.iter_mut().for_each(|x| *x = 0);
             buffer_offset = 0;
         }
-        // End of time calculation
-        //let duration = start.elapsed();
-        //println!("Time elapsed in expensive_function() is: {:?}", duration);
     }
 }
 
-fn server_bandwidth(cnt_bytes: usize) {
-    let mut string = "ololo";
-    println!("string: -------------- {}", cnt_bytes);
+fn server_bandwidth(cnt_bytes: usize, whole_bytes: usize) {
+    println!("{} bytes received in past five seconds.\n\
+              {} the whole number of bytes", cnt_bytes, whole_bytes);
 }
